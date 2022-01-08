2f6e4c0c19568945b52a5d03cee4ac633455d2da
NotANewt <meegafauna@gmail.com>
2022-01-08 15:33:57 -0600

    added functionality so that at least one of each of the special characters selected will be in the final password, then shuffled the password so the first characters would not be in order of the types chosen

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,99 +3,13 @@ let generateBtn = document.querySelector("#generate");
 
 //Arrays
 //lowercase characters
-let lcLetters = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
+let lcLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 //uppercase characters
-let ucLetters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
+let ucLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 //numeric characters
 let num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 //special characters
-let spChar = [
-  "!",
-  '"',
-  "'",
-  "#",
-  "$",
-  "%",
-  "&",
-  "(",
-  ")",
-  "*",
-  "+",
-  ",",
-  "-",
-  ".",
-  "/",
-  ":",
-  ";",
-  "<",
-  "=",
-  ">",
-  "?",
-  "@",
-  "[",
-  "\\",
-  "^",
-  "_",
-  "`",
-  "{",
-  "|",
-  "}",
-  "~",
-];
+let spChar = ["!", '"', "'", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "^", "_", "`", "{", "|", "}", "~"];
 
 // generatePassword
 // return a final password as a string
@@ -114,9 +28,7 @@ function generatePassword() {
   //This also validates that the value is a number.
   if (pwLength < minLength || pwLength > maxLength || pwLength % 1 !== 0) {
     //if not valid, pop up an alert box
-    alert(
-      "Cannot generate password: Please choose a whole number between 8 and 128."
-    );
+    alert("Cannot generate password: Please choose a whole number between 8 and 128.");
     //return nothing so a new value can be assigned
     return null;
   }
@@ -124,33 +36,22 @@ function generatePassword() {
   //Password Criteria
 
   //confirm whether to include lowercase characters
-  let requestLowerCase = confirm(
-    "Press OK to use lowercase letters in your password."
-  );
+  let requestLowerCase = confirm("Press OK to use lowercase letters in your password.");
 
   //confirm whether to include uppercase characters
-  let requestUpperCase = confirm(
-    "Press OK to use uppercase letters in your password."
-  );
+  let requestUpperCase = confirm("Press OK to use uppercase letters in your password.");
 
   //confirm whether to include numbers
   let requestNumbers = confirm("Press OK to use numbers in your password.");
 
   //confirm whether to include special characters
-  let requestSpecial = confirm(
-    "Press OK to use special characters in your password."
-  );
+  let requestSpecial = confirm("Press OK to use special characters in your password.");
 
   //if the user did not choose any characters
   //NOTE: by adding the values of all prompts, if all cancels are selected, the value will be 0
-  if (
-    requestLowerCase + requestUpperCase + requestNumbers + requestSpecial ===
-    0
-  ) {
+  if (requestLowerCase + requestUpperCase + requestNumbers + requestSpecial === 0) {
     //pop up an alert box to instruct them to pick at least one character type
-    alert(
-      "Cannot generate password: Please choose at least one character type."
-    );
+    alert("Cannot generate password: Please choose at least one character type.");
     //return nothing so a new value can be assigned
     return null;
   }
@@ -158,32 +59,49 @@ function generatePassword() {
   //if chose lowercase letters, add them to the pwArray
   if (requestLowerCase === true) {
     pwArray = pwArray.concat(lcLetters);
+    //to ensure this character class is in the final password, add one random lowercase letter
+    let randomItem = lcLetters[Math.floor(Math.random() * lcLetters.length)];
+    password.push(randomItem);
   }
 
   //if chose uppercase letters, add them to the pwArray
   if (requestUpperCase === true) {
     pwArray = pwArray.concat(ucLetters);
+    //to ensure this character class is in the final password, add one random uppercase letter
+    let randomItem = ucLetters[Math.floor(Math.random() * ucLetters.length)];
+    password.push(randomItem);
   }
 
   //if chose numberes, add them to the pwArray
   if (requestNumbers === true) {
     pwArray = pwArray.concat(num);
+    //to ensure this character class is in the final password, add one random number
+    let randomItem = num[Math.floor(Math.random() * num.length)];
+    password.push(randomItem);
   }
 
   //if chose special characters, add  them to the pwArray
   if (requestSpecial === true) {
     pwArray = pwArray.concat(spChar);
+    //to ensure this character class is in the final password, add one random special character
+    let randomItem = spChar[Math.floor(Math.random() * spChar.length)];
+    password.push(randomItem);
   }
 
   // create a pw with chosen length, then loop through the concatenated array
-  for (let i = 0; i < pwLength; i++) {
+  //first remove the number of characters already added from the final password length
+  let partialPasswordLength = pwLength - password.length;
+  //loop through the concatenated array to create the password
+  for (let i = 0; i < partialPasswordLength; i++) {
     // 1d. push each letter array into password array
     //1e. push a random letter from the array into password array
     password.push(pwArray[Math.floor(Math.random() * pwArray.length)]);
   }
+  //shuffle the password array so that the first characters are not in order of the types chosen
+  let finalPassword = password.sort((a, b) => 0.5 - Math.random());
 
-  //convert password array back to string
-  return password.join("");
+  //convert finalpassword array back to string
+  return finalPassword.join("");
 }
 
 // Write password to the #password input
